a2c1a32c730691c2adf19c2070b4db36fa7b12fd
Mohamed Elsaid <mohm2d@msn.com>
2022-06-01 15:32:30 +0300

    Add files via upload
    this is a test file

---
+++ 01.py
@@ -0,0 +1,71 @@
+
+# Python is a case sensitive language. 
+# Variables : is a name that refers to a value
+# we wirte it like this ===> Variable = value
+# Variable types ( int - string - float - boolean - list - tuple - dictionary )
+
+# assignment variable
+x = 5     # Intger
+y = 4.5   # Float
+
+# Multi assignment
+
+a,b = True, "Mohamed"
+
+c = d = f = 9
+
+print (x)
+print (y)
+print (a,b)
+print (c,d,f)
+
+# you can use ; if you want to code in one line
+
+print(type (x)) ; print(type(y)) ; print(type (a)) ; print(type(b))
+
+# Operators
+# Arithmatic operators like ====>
+#( + << Addition , - << Subtraction ,* << Multiplication , / << Division )
+# ( % << Modulus , // << Floor division , ** << exponent )
+
+
+# Comparison operators
+# ( < less than ,  > greater than  , == equal to , != not equal )
+# ( <=  less than or equal , >= greater than or equal )
+
+
+# Assignment operators
+# ( +=  , -=  , *=  , **=  , /=  )
+
+k = 4
+
+k **= 2
+
+print (k)
+
+# Operator precdence  ترتيب العمليات الحسابية 
+# () , ** , * / , = -
+
+
+
+# Help Method
+
+# Conditions   If Condition
+s = 4
+if s == 4 :
+    print ("S = ",s)
+# and <<< must have all conditions True
+if c == d and d == f :
+    print ("C , D and F is matching")
+# or <<< if you have one condition True
+# if and else
+if a == f or f == b :
+    print ("True")
+else:
+    print ("False")
+
+# if , elif and else
+
+
+
+
